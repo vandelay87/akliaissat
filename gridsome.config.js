0f48bd364d358d1fbd36cb4afa0624c61d879e52
Akli Aissat <akli.aissat@whitbread.co.uk>
2019-04-15 16:53:59 +0100

    fix analytics and update dependancy versions

--- gridsome.config.js
+++ gridsome.config.js
@@ -10,9 +10,6 @@ module.exports = {
         id: process.env.AKLIAISSAT_ANALYTICS_KEY
           ? process.env.AKLIAISSAT_ANALYTICS_KEY
           : 'GA_TRACKING_ID',
-        autoTracking: {
-          exception: true
-        },
         debug: {
           enabled: !isProd,
           sendHitTask: isProd
