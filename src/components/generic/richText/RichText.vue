423eeeee1d7a0496ce3fe346408da658019b51e6
vandelay87 <akliaissat@outlook.com>
2019-10-02 23:11:54 +0100

    finish basic richText cin

--- src/components/generic/richText/RichText.vue
+++ src/components/generic/richText/RichText.vue
@@ -1,9 +1,10 @@
 <template>
-  <article v-html="richtextToHTML(article)" class="mdc-typography mdc-typography--body1"></article>
+  <article v-html="richtextToHTML(article)" class="mdc-typography mdc-typography--body1" />
 </template>
 
 <script>
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
 
 export default {
   name: "RichText",
@@ -13,9 +14,21 @@ export default {
       required: true
     }
   },
+  computed: {
+    options() {
+      return {
+        renderNode: {
+          [BLOCKS.HEADING_1]: node =>
+            `<h1 class="mdc-typography--headline1">${node.content[0].value}</h1>`,
+          [BLOCKS.HEADING_2]: node =>
+            `<h2 class="mdc-typography--headline2">${node.content[0].value}</h2>`
+        }
+      };
+    }
+  },
   methods: {
-    richtextToHTML(content) {
-      return documentToHtmlString(content);
+    richtextToHTML(document) {
+      return documentToHtmlString(document, this.options);
     }
   }
 };
